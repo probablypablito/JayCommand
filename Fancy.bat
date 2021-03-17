a7302cb316128960e1ecbac99d8eb3ea60ead2ce
probablypablito <55856800+probablypablito@users.noreply.github.com>
2021-03-17 11:04:35 -0400

    Add files via upload

---
+++ Fancy.bat
@@ -0,0 +1,4 @@
+::::: GENERATED WITH NOOBS CMDR v1.0.2.0 :::::
+chcp 65001
+
+OBSCommand.exe /server=127.0.0.1:4444 /password="" /showsource="ALL CHANNEL POINTS"/"Fancy" /delay=3 /hidesource="ALL CHANNEL POINTS"/"Fancy"
